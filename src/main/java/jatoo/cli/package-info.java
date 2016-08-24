40e18edab149a8d17c7691e91d17fa2c6dac794c
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-08-24 14:52:10 +0300

    package-info.java

---
+++ src/main/java/jatoo/cli/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * A Java™ Open Source library created to expose as commands the tools and
+ * utilities from the JaToo libraries.
+ * 
+ * @author <a href="http://cristian.sulea.net" rel="author">Cristian Sulea</a>
+ */
+package jatoo.cli;
